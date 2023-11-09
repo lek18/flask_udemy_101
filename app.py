28b2bc0422ff7cfffb97c2769ad8b772c9d5c4a2
Luis Ek <Luis.Ek.-ND@disney.com>
2023-11-09 16:47:46 -0600

    final version of cdoe

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
+stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
 
 
 @app.get("/store")
@@ -25,5 +25,21 @@ def create_item(name):
         if store["name"] == name:
             new_item = {"name": request_data["name"], "price": request_data["price"]}
             store["items"].append(new_item)
-            return new_item
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
     return {"message": "Store not found"}, 404
